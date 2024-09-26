654c97c766f088ed7a70861f86d6a806b906d925
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-09-26 19:58:44 +0200

    Commit inicial

---
+++ src/main/java/com/mireyaserrano/tema03/Main.java
@@ -0,0 +1,15 @@
+package com.mireyaserrano.tema03;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Indica un número: ");
+        int i = Integer.parseInt(scanner.nextLine());
+        System.out.println("Hello world!");
+        int x = i++;
+        int y = ++i;
+
+    }
+}
