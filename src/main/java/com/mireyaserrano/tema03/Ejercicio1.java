127bdbc32dc7ba7c6e4f6fdc03e99db0c084a44c
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-09-30 20:48:04 +0200

    Añadimos un  saludo

--- src/main/java/com/mireyaserrano/tema03/Ejercicio1.java
+++ src/main/java/com/mireyaserrano/tema03/Ejercicio1.java
@@ -3,5 +3,6 @@ package com.mireyaserrano.tema03;
 public class Ejercicio1 {
     public static void main(String[] args) {
         System.out.println("Ejercicio1");
+        System.out.println("Hola caracola");
     }
 }
