ca6d52a5125ca4ee023c9e4bb484366289a82ace
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-10-01 16:17:07 +0200

    Realizado el ejercicio1 del tema03

--- src/main/java/com/mireyaserrano/tema03/Ejercicio1.java
+++ src/main/java/com/mireyaserrano/tema03/Ejercicio1.java
@@ -1,8 +1,17 @@
 package com.mireyaserrano.tema03;
 
+import java.util.Scanner;
 public class Ejercicio1 {
-    public static void main(String[] args) {
-        System.out.println("Ejercicio1");
-        System.out.println("Hola caracola");
+    public static void main (String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduzca un número:");
+        int num = Integer.parseInt(scanner.nextLine());
+        scanner.close();
+
+        if (num % 2 == 0){
+            System.out.println("El número es par");
+        }else{
+            System.out.println("El número es impar");
+        }
     }
 }
