4ee3c94d90d3e5b30e1002ea93781fdf1a514429
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-10-02 16:07:10 +0200

    Switch mas bonito

--- src/main/java/com/mireyaserrano/tema03/Ejercicio5.java
+++ src/main/java/com/mireyaserrano/tema03/Ejercicio5.java
@@ -10,25 +10,17 @@ public class Ejercicio5 {
         scanner.close();
         if (nota>=0 && nota<11){
             switch (nota){
-                case 0: case 1: case 2: case 3: case 4:
-                    System.out.println("INSUFICIENTE");
-                    break;
-
-                case 5:
-                    System.out.println("SUFICIENTE");
-                    break;
-
-                case 6:
-                    System.out.println("BIEN");
-                    break;
-
-                case 7: case 8:
-                    System.out.println("NOTABLE");
-                    break;
+                case 0, 1, 2, 3, 4-> System.out.println("INSUFICIENTE");
 
 
-                case 9: case 10:
-                    System.out.println("SOBRESALIENTE");
+                case 5->System.out.println("SUFICIENTE");
+
+                case 6->System.out.println("BIEN");
+
+                case 7, 8->System.out.println("NOTABLE");
+
+
+                case 9, 10->System.out.println("SOBRESALIENTE");
 
 
             }
