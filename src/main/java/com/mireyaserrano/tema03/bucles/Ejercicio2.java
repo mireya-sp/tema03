8ff5b62f22a586bb8b3a33d40c070920e800c931
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-10-04 20:45:33 +0200

    Ej2 bucles

---
+++ src/main/java/com/mireyaserrano/tema03/bucles/Ejercicio2.java
@@ -0,0 +1,11 @@
+package com.mireyaserrano.tema03.bucles;
+
+public class Ejercicio2 {
+    public static void main(String args[]){
+        int num = 0;
+        for (int i = 0; i<=1000; i++){
+            num = num + i;
+        }
+        System.out.println(num);
+    }
+}
