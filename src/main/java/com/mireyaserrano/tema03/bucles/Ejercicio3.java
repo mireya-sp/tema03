0887cc84e29414d96deeffbec15c9346eeb239e4
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-10-04 20:55:11 +0200

    Ej3 bucles

---
+++ src/main/java/com/mireyaserrano/tema03/bucles/Ejercicio3.java
@@ -0,0 +1,13 @@
+package com.mireyaserrano.tema03.bucles;
+
+public class Ejercicio3 {
+    public static void main(String args[]){
+        int num = 5;
+        int num2 = 8;
+        int total = num;
+        for (int i = 1; i < num2; i++){
+            total = total + num;
+        }
+        System.out.println(total);
+    }
+}
