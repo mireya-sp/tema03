1a443f5f2e8467aac240e71a5d9d89b2b2cfd3ee
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-10-07 20:18:09 +0200

    Ahora se pueden multiplicar con números negativos

--- src/main/java/com/mireyaserrano/tema03/bucles/Ejercicio3.java
+++ src/main/java/com/mireyaserrano/tema03/bucles/Ejercicio3.java
@@ -1,13 +1,26 @@
 package com.mireyaserrano.tema03.bucles;
 
+import java.util.Scanner;
+
 public class Ejercicio3 {
     public static void main(String args[]){
-        int num = 5;
-        int num2 = 8;
-        int total = num;
-        for (int i = 1; i < num2; i++){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce un número");
+        int num = Integer.parseInt(scanner.nextLine());
+        System.out.println("Introduce el segundo número");
+        int num2 = Integer.parseInt(scanner.nextLine());
+        int total = 0;
+        int signoA, signoB;
+        scanner.close();
+
+        signoA = num < 0 ? -1 : 1;
+        num *= signoA;
+        signoB = num2 < 0 ? -1 : 1;
+        num2 *= signoB;
+        for (int i = 1; i <= num2; i++){
             total = total + num;
         }
+        total = total * signoA * signoB;
         System.out.println(total);
     }
 }
