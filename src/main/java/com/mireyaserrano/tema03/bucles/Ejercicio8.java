784bc1a444bf478b417301d05ffed905cd8f081c
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-10-08 15:16:20 +0200

    Ej8 bucles

--- src/main/java/com/mireyaserrano/tema03/bucles/Ejercicio8.java
+++ src/main/java/com/mireyaserrano/tema03/bucles/Ejercicio8.java
@@ -1,4 +1,18 @@
 package com.mireyaserrano.tema03.bucles;
 
+import java.util.Scanner;
+
 public class Ejercicio8 {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduzca un número y le devuelve el factorial: ");
+        int num = Integer.parseInt(scanner.nextLine());
+        int total = 1;
+        scanner.close();
+
+        for (int i = 1; i <= num; i++){
+            total = total * i;
+        }
+        System.out.print("El factorial de " + num + " es " + total);
+    }
 }
