2f78140c57e562e34c9da5ca5661def3b4246dc4
mireya-sp <mireyaserrano181105@gmail.com>
2024-10-15 15:43:50 +0200

    Ej4 salida con formato

--- src/main/java/com/mireyaserrano/tema03/anexo2/Ejercicio4.java
+++ src/main/java/com/mireyaserrano/tema03/anexo2/Ejercicio4.java
@@ -2,11 +2,10 @@ package com.mireyaserrano.tema03.anexo2;
 
 public class Ejercicio4 {
     public static void main(String args[]){
+        String cadena = "";
         for (int i =1; i <= 9; i++){
-            for(int j = 1; j <= i; j++){
-                System.out.printf("%d", j);
-            }
-            System.out.printf("%n");
+            cadena = cadena + i;
+            System.out.println(cadena);
         }
     }
 }
