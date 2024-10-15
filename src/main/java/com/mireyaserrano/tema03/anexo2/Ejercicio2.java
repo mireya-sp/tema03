c9cbddbf59b4c8b6cd66f3407af88e7afee6c94b
mireya-sp <mireyaserrano181105@gmail.com>
2024-10-15 15:34:10 +0200

    Ej2 salida con formato

--- src/main/java/com/mireyaserrano/tema03/anexo2/Ejercicio2.java
+++ src/main/java/com/mireyaserrano/tema03/anexo2/Ejercicio2.java
@@ -6,21 +6,21 @@ public class Ejercicio2 {
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
         int h, m, s;
-        boolean pepa = true;//me gusta llamar a mi variable auxiliar pepa, se que seria mejor llamrla valido.
+        boolean validar = true;
         do {
             System.out.println("Introduzca las horas:");
             h = Integer.parseInt(scanner.nextLine());
             System.out.println("Introduzca los minutos:");
-             m = Integer.parseInt(scanner.nextLine());
+            m = Integer.parseInt(scanner.nextLine());
             System.out.println("Introduzca los segundos:");
             s = Integer.parseInt(scanner.nextLine());
-            if (h < 24 && h > -1 && m < 61 && m > -1 && s < 61 && s > -1) {
+            if (h < 24 && h > -1 && m < 60 && m > -1 && s < 60 && s > -1) {
                 System.out.printf("Las horas, minutos y segundos en formato hh:mm:ss son -> %02d:%02d:%02d", h, m, s);
-                pepa = true;
+                validar = true;
             }else {
                 System.err.println("ERROR. Introduzca valores adecuados para horas, minutos y segundos.");
-                pepa = false;
+                validar = false;
             }
-        }while (!pepa);
+        }while (!validar);
     }
 }
