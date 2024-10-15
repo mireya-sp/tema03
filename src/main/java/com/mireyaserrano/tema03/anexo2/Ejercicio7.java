ade23b83436d945f8fbfbcbfb7da529546dad443
mireya-sp <mireyaserrano181105@gmail.com>
2024-10-15 17:20:39 +0200

    Ej7 salida con formato

--- src/main/java/com/mireyaserrano/tema03/anexo2/Ejercicio7.java
+++ src/main/java/com/mireyaserrano/tema03/anexo2/Ejercicio7.java
@@ -1,4 +1,15 @@
 package com.mireyaserrano.tema03.anexo2;
 
 public class Ejercicio7 {
+    public static void main(String args[]){
+        String cadena = "";
+        String cadena2 = "";
+        for (int i =1; i <= 9; i++){
+            cadena = cadena + i;
+            for (int j = 1; j <= 1; j++){
+                cadena2 = i + cadena2;
+            }
+            System.out.printf("%9s%s\n", cadena, cadena2);
+        }
+    }
 }
