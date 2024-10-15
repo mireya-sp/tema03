86425eccf78d6bf726dc5c8539bd708efb39de7b
Mireya Serrano <mireyaserrano181105@gmail.com>
2024-10-15 12:53:58 +0200

    Ej4 salida con formato

---
+++ src/main/java/com/mireyaserrano/tema03/anexo2/Ejercicio4.java
@@ -0,0 +1,12 @@
+package com.mireyaserrano.tema03.anexo2;
+
+public class Ejercicio4 {
+    public static void main(String args[]){
+        for (int i =1; i <= 9; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.printf("%d", j);
+            }
+            System.out.printf("%n");
+        }
+    }
+}
