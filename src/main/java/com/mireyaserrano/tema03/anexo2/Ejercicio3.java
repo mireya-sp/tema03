b2c67e886278c9140189730125da4e1d66ac12c1
mireya-sp <mireyaserrano181105@gmail.com>
2024-10-15 15:45:47 +0200

    Ej3 salida con formato

--- src/main/java/com/mireyaserrano/tema03/anexo2/Ejercicio3.java
+++ src/main/java/com/mireyaserrano/tema03/anexo2/Ejercicio3.java
@@ -1,4 +1,13 @@
 package com.mireyaserrano.tema03.anexo2;
 
+import java.util.Arrays;
+
 public class Ejercicio3 {
+    public static void main(String args[]){
+        String cadena = "";
+        for (int i =1; i <= 9; i++){
+            cadena = cadena + i;
+            System.out.printf("%9s\n",cadena);
+        }
+    }
 }
