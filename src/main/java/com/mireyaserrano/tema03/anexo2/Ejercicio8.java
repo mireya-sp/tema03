3276022a7724cca40bf26f6a35bba35eadde418b
mireya-sp <mireyaserrano181105@gmail.com>
2024-10-16 15:51:28 +0200

    Ej8 salida con formato

---
+++ src/main/java/com/mireyaserrano/tema03/anexo2/Ejercicio8.java
@@ -0,0 +1,24 @@
+package com.mireyaserrano.tema03.anexo2;
+
+public class Ejercicio8 {
+    public static void main(String args[]){
+        int colorl = 30;
+        int colorf = 40;
+        int intensidad = 0;
+        String cadena;
+        for (int k = 1; k <= 2; k++){
+            for (int j = 1; j <= 8; j++) {
+                colorf = 40;
+                for (int i = 1; i <= 8; i++) {
+                    cadena = intensidad + ";" + colorl + ";" + colorf;
+                    System.out.printf("\u001B[" + intensidad + ";" + colorl + ";" + colorf + "m" + "%8s" + "\u001B[0m", cadena);
+                    colorf += 1;
+                }
+                System.out.println("");
+                colorl += 1;
+            }
+            colorl = 30;
+            intensidad += 1;
+        }
+    }
+}
