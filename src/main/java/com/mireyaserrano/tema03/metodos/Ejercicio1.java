474778977a8a745548a41539ec1390d1617a947f
mireya-sp <mireyaserrano181105@gmail.com>
2024-10-22 15:17:56 +0200

    Ej1 metodos

---
+++ src/main/java/com/mireyaserrano/tema03/metodos/Ejercicio1.java
@@ -0,0 +1,11 @@
+package com.mireyaserrano.tema03.metodos;
+
+public class Ejercicio1 {
+    public static int sumar(int x, int y){
+        return x + y;
+    }
+    public static void main(String args[]){
+        int resultado = sumar(5,3);
+        System.out.println(resultado);
+    }
+}
